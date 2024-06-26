15e199dc8628d2424f60f4688d41152674dc7157
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-06-26 14:07:42 +1000

    cat movement

--- entities/cat.gd
+++ entities/cat.gd
@@ -1,14 +1,43 @@
 class_name Cat extends Area2D
 
-var moving : bool = true
-var speed : float = 10
-var direction : int = 1
+
+const PLATFORM_MARGIN = 16
+const N_PLATFORMS = 3
 
 
-func _process(delta):
-	if moving:
-		position.x += speed * direction * delta
-		
+var moving: bool = true
+var jump := false
+@export var speed: float = 50
+@export var direction: int = 1
+@export var platform: int = 1
+
+@onready var check_holes: Area2D = $CheckHoles
+
 
 func change_direction():
 	direction *= -1
+	scale.x = direction
+
+
+func has_platform_below() -> bool:
+	for area: Area2D in check_holes.get_overlapping_areas():
+		if area.is_in_group("Platform"):
+			return true
+	return false
+
+
+func _physics_process(delta):
+	if moving:
+		position.x += speed * direction * delta
+	
+	if Input.is_action_just_pressed("up") and platform < N_PLATFORMS - 1:
+		platform += 1
+		position.y -= PLATFORM_MARGIN
+		jump = true
+	elif Input.is_action_just_pressed("down") and platform > 0:
+		platform -= 1
+		position.y += PLATFORM_MARGIN
+		jump = true
+	
+	if not has_platform_below():
+		change_direction()
